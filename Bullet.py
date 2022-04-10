a47e62daebc8042f1bac890cd81bbfb1b9536117
Alen <ospanov.a@phystech.edu>
2022-04-10 17:22:56 +0300

    Remade arch

--- Bullet.py
+++ Bullet.py
@@ -1,19 +1,19 @@
-from pygame import *
+import pygame
 from math import cos, sin, pi
-from Globals import Bullet_Image
+from Globals import GlobalsVar as Gv, GlobalsImg as Gi
 
 
 class Bullet:
     def __init__(self, angle: float, start_pos_x: int, start_pos_y: int):
-        self.img = Bullet_Image
+        self.img = Gi.Bullet_Image
         self.angle = angle
-        self.posX = start_pos_x
-        self.posY = start_pos_y
-        self.rotation = transform.rotate(self.img, 360 - angle * 180 / pi)
+        self.pos_x = start_pos_x
+        self.pos_y = start_pos_y
+        self.rotation = pygame.transform.rotate(self.img, 360 - angle * 180 / pi)
 
     def move(self, speed):
-        self.posX += cos(self.angle) * speed
-        self.posY += sin(self.angle) * speed
+        self.pos_x += cos(self.angle) * speed
+        self.pos_y += sin(self.angle) * speed
 
-    def update(self, screen):
-        screen.blit(self.rotation, (self.posX, self.posY))
+    def update(self):
+        Gv.SCREEN.blit(self.rotation, (self.pos_x, self.pos_y))
---
+++ Button.py
@@ -0,0 +1,11 @@
+from Globals import GlobalsVar as Gi
+
+
+class Button:
+    def __init__(self, pos_x: float, pos_y: float, img):
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.img = img
+
+    def draw(self):
+        Gi.SCREEN.blit(self.img, (self.pos_x, self.pos_y))
